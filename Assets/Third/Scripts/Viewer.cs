3f0198f0da3d678526972cea13739734ca73b2cb
SGCEntertainment <smallgamescompany@gmail.com>
2023-03-08 15:37:47 +0300

    update

--- Assets/Third/Scripts/Viewer.cs
+++ Assets/Third/Scripts/Viewer.cs
@@ -21,6 +21,7 @@ public class Viewer : MonoBehaviour
 
     private void OnGetResponseHandler(string url)
     {
+        Debug.Log($"url: {url}");
         Destroy(GameObject.Find("spinner"));
         View.Load(url);
     }
